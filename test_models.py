474594ec551120e477fd1f7fe37f7d39921d7162
Kevin Chavez <kjc47@njit.edu>
2023-11-03 02:01:30 -0400

    bugs

--- test_models.py
+++ test_models.py
@@ -1,34 +1,46 @@
-import os
+import datetime
+from models import create_tables, UserModel, engine, Session, seed_users
+from factories import user_factory
 import pytest
-from models import create_tables, seed_users, UserModel, engine, Session
 
-# Setup and Teardown methods
-@pytest.fixture(scope='module')
-def setup_module():
-    # Set the DATABASE_URL to point to a test database
-    os.environ["DATABASE_URL"] = 'sqlite:///test_models.db'
-    
-    # Create tables and seed users for tests
+@pytest.fixture(scope="module")
+def db_session():
+    # Create tables
     create_tables()
+
+    # Create a new session
+    session = Session()
+
+    yield session  # this is where the testing happens
+
+    session.close()
+    # Drop all tables after tests
+    UserModel.__table__.drop(engine)
+
+def test_user_creation(db_session):
+    user = user_factory(UserModel)
+    db_session.add(user)
+    db_session.commit()
+
+    fetched_user = db_session.query(UserModel).first()
+    assert fetched_user is not None
+    assert fetched_user.first_name == user.first_name
+    assert fetched_user.last_name == user.last_name
+
+def test_full_name_property(db_session):
+    user = UserModel(first_name="John", last_name="Doe")
+    assert user.full_name == "John Doe"
+
+def test_created_field(db_session):
+    now = datetime.datetime.utcnow()
+    user = UserModel(first_name="Jane", last_name="Doe")
+    db_session.add(user)
+    db_session.commit()
+
+    user_from_db = db_session.query(UserModel).filter_by(first_name="Jane").first()
+    assert user_from_db.created >= now
+
+def test_seed_users(db_session):
     seed_users()
-    
-    yield
-
-    # Cleanup: Close the engine and remove the test database file
-    engine.dispose()
-    if os.path.exists('test_models.db'):
-        os.unlink('test_models.db')
-
-# Test functions
-def test_user_count(setup_module):
-    session = Session()
-    user_count = session.query(UserModel).count()
-    session.close()
-    assert user_count >= 2, "Should have at least two seeded users"
-
-def test_user_full_name(setup_module):
-    session = Session()
-    user = session.query(UserModel).first()
-    full_name = user.full_name
-    session.close()
-    assert ' ' in full_name, "Full name should have a space between first and last name"
+    user_count = db_session.query(UserModel).count()
+    assert user_count >= 2
