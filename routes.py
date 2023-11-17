83ec38274345761ae645e21d8bb0fdcd38bdcfad
Kevin Chavez <kjc47@njit.edu>
2023-11-17 06:02:24 -0500

    update

--- routes.py
+++ routes.py
@@ -1,8 +1,11 @@
-from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from models import UserModel, TodoModel, SessionLocal, engine
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import scoped_session, sessionmaker
+from forms import TodoForm
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24).hex()  # Set a random SECRET_KEY for CSRF protection
 
 # Set up the session for the database
 session_factory = sessionmaker(bind=engine)
@@ -25,47 +28,53 @@ def get_user(user_id):
     user = session.query(UserModel).get(user_id)
     Session.remove()
     if user:
-        return jsonify(user.full_name)
+        return render_template('user_view.html', user=user)
     else:
-        return jsonify(error="User not found"), 404
+        flash('User not found', 'error')
+        return redirect(url_for('index'))
 
 # Todo Routes
 @app.route('/todos/create', methods=['GET', 'POST'])
 def create_todo():
     """Create a new todo item."""
-    if request.method == 'POST':
-        title = request.form['title']
-        description = request.form['description']
-        new_todo = TodoModel(title=title, description=description)
+    form = TodoForm()
+    if form.validate_on_submit():
         session = Session()
+        new_todo = TodoModel(title=form.title.data, description=form.description.data)
         session.add(new_todo)
         session.commit()
         Session.remove()
+        flash('Todo created successfully!', 'success')
         return redirect(url_for('index'))
-    return render_template('create_todo.html')
+    return render_template('create_todo.html', form=form)
 
 @app.route('/todos/update/<int:todo_id>', methods=['GET', 'POST'])
 def update_todo(todo_id):
     """Update an existing todo item."""
-    session = SessionLocal()
-    todo = session.get(TodoModel, todo_id)
-    if request.method == 'POST':
-        if todo:
-            todo.title = request.form['title']
-            todo.description = request.form['description']
-            session.commit()
+    session = Session()
+    todo = session.query(TodoModel).get(todo_id)
+    form = TodoForm(obj=todo)
+    if form.validate_on_submit():
+        todo.title = form.title.data
+        todo.description = form.description.data
+        session.commit()
         Session.remove()
+        flash('Todo updated successfully!', 'success')
         return redirect(url_for('index'))
-    return render_template('update_todo.html', todo=todo)
+    Session.remove()
+    return render_template('update_todo.html', form=form, todo_id=todo_id)
 
 @app.route('/todos/delete/<int:todo_id>', methods=['POST'])
 def delete_todo(todo_id):
     """Delete a todo item."""
     session = Session()
-    todo = session.get(TodoModel, todo_id)
+    todo = session.query(TodoModel).get(todo_id)
     if todo:
         session.delete(todo)
         session.commit()
+        flash('Todo deleted successfully!', 'success')
+    else:
+        flash('Todo not found', 'error')
     Session.remove()
     return redirect(url_for('index'))
 
@@ -73,9 +82,13 @@ def delete_todo(todo_id):
 def get_todo(todo_id):
     """Retrieve a single todo item."""
     session = Session()
-    todo = Session.get(TodoModel, todo_id)
+    todo = session.query(TodoModel).get(todo_id)
     Session.remove()
-    return render_template('todo_view.html', todo=todo)
+    if todo:
+        return render_template('todo_view.html', todo=todo)
+    else:
+        flash('Todo not found', 'error')
+        return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
